2173aa88120f048a2d0e526025d2ae12cf98f4ae
mandragora-hub <royer01gomez@gmail.com>
2026-08-06 20:16:48 -0400

    refactor: move at interfaces to at folder

--- main/utils.h
+++ main/utils.h
@@ -4,7 +4,7 @@
 #define TRACCAR_URL "https://osmand.traccar.redania.online"
 #define DEVICE_ID "12345"
 
-#include "gnss.h"
+#include "at/gnss.h"
 
 const char *build_osmand_traccar_url(char *dest_url, size_t dest_url_size, gnss_info_t *gnss_info);
 void remaining_task_stack();
