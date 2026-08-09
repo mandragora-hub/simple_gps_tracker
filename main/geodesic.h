9fd6c21dfa6c01fea370add1c1644b987cc8c5b6
mandragora-hub <royer01gomez@gmail.com>
2026-08-09 10:30:25 -0400

    refactor: add references

--- main/geodesic.h
+++ main/geodesic.h
@@ -1,6 +1,10 @@
 #ifndef GEODESIC_H
 #define GEODESIC_H
 
+//https://www.studysmarter.es/resumenes/ingenieria/ingenieria-tecnologia-minera/distancia-geodesica/
+//https://es.wikipedia.org/wiki/Ley_esf%C3%A9rica_de_los_cosenos
+//https://en.wikipedia.org/wiki/Haversine_formula
+
 #include "at/gnss.h"
 
 double calculate_geodesic_distances_gnss(gnss_info_t *location1, gnss_info_t *location2);
